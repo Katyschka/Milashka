8420e1f1e5caaf64575b905c2558e9166f13a71e
Катюша <Milaya-katyscha@mail.ru>
2022-03-30 19:17:36 +0300

    новый класс

---
+++ ty2.py
@@ -0,0 +1,9 @@
+a=[1,5,0,2,7,9]
+g=[]
+j=0
+if min(a):
+    a.remove(i)
+print(a)
+g.insert(0,i)
+print(g)
+
---
+++ тор.py
@@ -0,0 +1,11 @@
+a=[2,3,4,5,4,3,1]
+g=[]
+o=0
+for t in a:
+    if a.count(t)==2:
+        g.append(t)
+# print(a.count(len(g)))
+for f in g:
+    print(f)
+        d=dict(zip[g.count(f)],)
+    
